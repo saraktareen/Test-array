93c2e156294feb52c90fa3c4fc3de2599a932aea
Sara Khan Tareen <75300313+saraktareen@users.noreply.github.com>
2022-10-08 13:04:13 +0500

    Add files via upload

---
+++ array.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+public class array 
+{
+        public static void main (String[] args) 
+        {
+            Random random = new Random();
+            int[] arr = new int [10]; //initialise array
+            
+            //for loop to enter values into the array
+            for (int i=0; i < arr.length ; i++)
+            {
+                arr[i] = random.nextInt(50);
+            }
+
+            //for loop to display all the values of the array
+            for (int i=0; i <arr.length; i++)
+            {
+                System.out.printf("A[" + i + "] = " + arr[i] + "%n");
+            }
+        }
+    }
+    
